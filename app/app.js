603ad884842d0fde4d28a3451aafdb2f5638f06f
hitamu <hitamu@live.com>
2016-02-25 20:54:53 +0700

    Change to stable version

--- app/app.js
+++ app/app.js
@@ -1,9 +1,2 @@
 'use strict';
 var izNo = angular.module('izNo', ['monospaced.elastic']);
-
-function save(text, url) {
-  var scope = angular.element(document.getElementById("izNote")).scope();
-  scope.$apply(function () {
-      scope.save(text, url);
-  });
-}
--- background.js
+++ background.js
@@ -1,15 +1,3 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
     chrome.tabs.create({ url: "index.html" });
 });
-
-chrome.runtime.onInstalled.addListener(function() {
-    chrome.contextMenus.create({
-        title: 'Quick save to izNote',
-        id: 'default',
-        contexts: ['selection']
-    });
-});
-
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  save(info.selectionText, info.pageUrl);
-});
